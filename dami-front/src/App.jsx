ca8265ffbd7f32d7c3dbedb7a22a07d11a9f7598
masxkip <jesufemiketiku@gmail.com>
2025-08-25 05:56:41 -0700

    React frontend ready for Vercel

---
+++ dami-front/src/App.jsx
@@ -0,0 +1,31 @@
+// src/App.jsx
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SiteLayout from "./layouts/SiteLayout";
+import Home from "./pages/Home";
+import Quote from "./pages/Quote";
+import Contact from "./pages/Contact";
+import Service from "./pages/Service";
+import About from "./pages/About";
+import Fqs from "./pages/Fqs";
+import ScrollToTop from "./components/ScrollToTop";
+
+function App() {
+  return (
+    <Router>
+      
+
+      <Routes>
+        <Route element={<SiteLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/quote" element={<Quote />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/services" element={<Service />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/faq" element={<Fqs />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
